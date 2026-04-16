af004904420916b549f26bacab4aa32df0086aee
beatrizpandolfi <beatrizpmaroja@gmail.com>
2026-04-16 11:46:26 -0300

    Início da escrita do código da classe Produto

---
+++ backend/include/Produto.h
@@ -0,0 +1,28 @@
+#ifndef PRODUTO_H
+#define PRODUTO_H
+
+#include <string>
+using namespace std;
+
+class Produto{
+    private:
+        // Atributos
+        int id;
+        string nome_produto;
+        double preco;
+        string categoria;
+
+    public:
+        // Construtor 
+        Produto(int id = 0, string nome = "", double p = 0.0, string c = "");
+
+        // Métodos
+        string getNome() const;
+        double getPreco() const;
+        string getCategoria() const;
+        void setPreco(double novoPreco);
+        string toString();
+
+};
+
+#endif
---
+++ backend/src/Produto.cpp
@@ -0,0 +1,49 @@
+#include "Produto.h"
+#include <string>
+
+using namespace std;
+
+Produto::Produto(int id, string nome, double p, string c){
+    this->id = id; // this->id atributo da classe
+    
+    nome_produto = nome;
+
+    if (p < 0)
+        preco = 0.0;
+    else
+        preco = p;
+    
+    categoria = c;
+
+}
+
+// Função para colocar nome do produto
+string Produto::getNome() const{
+    return nome_produto;
+}
+
+// Função para colocar preço do produto
+double Produto::getPreco() const{
+    return preco;
+}
+
+// Função para colocar categoria do produto
+string Produto::getCategoria() const{
+    return categoria;
+}
+
+// Função para validar e atualizar o preço
+void Produto::setPreco(double novoPreco){
+    if (novoPreco >= 0){
+        preco = novoPreco;
+    }
+}
+
+// Função para retornar uma descrição formatada
+string Produto::toString(){
+    return "Produto: " + nome_produto +
+            " | Preco: " + to_string(preco) + // "Preço na forma de string"
+            " | Categoria: " +categoria;
+}
+
+
