4e41966d06453d0dbddf2b109e5c9b0ed5326ca8
Luisa <luisalocio07@gmail.com>
2026-04-16 13:10:59 -0300

    Inicia implementação da classe Estoque

---
+++ backend/include/Estoque.h
@@ -0,0 +1,24 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+#include <string>
+#include <vector>
+#include "Produto.h"
+
+using namespace std;
+
+// Classe responsável por gerenciar o estoque de produtos
+class Estoque{
+    private:
+        // Vetor que armazena os produtos cadastrados
+        vector<Produto> produtos;
+
+    public:
+        // Métodos
+        void adicionarProduto(const Produto& p);
+        void listarProdutos();
+        Produto* buscarProduto(const string& nome);
+
+};
+
+#endif
---
+++ backend/src/Estoque.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../include/Estoque.h"
+
+using namespace std;
+
+// Função para adicionar um produto ao estoque
+void Estoque::adicionarProduto(const Produto& p){
+    produtos.push_back(p);
+}
+
+// Função para listar todos os produtos do estoque
+void Estoque::listarProdutos(){
+    if (produtos.size() == 0){
+        cout << "Nenhum produto cadastrado." << endl;
+    }
+    else{
+        // Percorre todos os produtos do vetor
+        for (int i = 0; i < produtos.size(); i++){
+        cout << produtos[i].toString() << endl;
+        }
+    }
+}
+
+// Função para buscar um produto pelo nome
+Produto* Estoque::buscarProduto(const string& nome){
+    // Percorre todos os produtos
+    for (int i = 0; i < produtos.size(); i++){
+        // Compara o nome informado com o nome do produto
+        if (nome == produtos[i].getNome()){
+            return &produtos[i];
+        }
+    }
+    // Retorna caso não encontre o produto
+    return nullptr;
+}
